e153d786046e17287e3843ff7c06e86b7fd3f845
Seongchan Lee <sungchan.lee06@gmail.com>
2024-08-01 16:55:34 -0700

    Delete framerusercontent.com directory

--- framerusercontent.com/sites/5QkobOnvzUbALdmcSzW6VY/chunk-M2URXET2.mjs
+++
@@ -1,2 +0,0 @@
-function a(t,e){return{description:"While Brian may not own any physical houses, he has an internet home at least. ",favicon:"https://framerusercontent.com/assets/ciBGjLnbjXZp5aFxUxvvwaZNig.png",robots:"max-image-preview:large",socialImage:"https://framerusercontent.com/assets/dIWI8EpGIpYMYnF8VAPPWR6ENfg.png",title:"Meet Brian!"}}export{a};
-//# sourceMappingURL=chunk-M2URXET2.mjs.map
